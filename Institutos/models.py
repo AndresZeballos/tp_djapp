a0e40d848f354a44ad8e8371630075fa09359bce
Andrés Zeballos <andres@topsystemscorp.com>
2018-04-17 19:20:44 -0300

    Modificaciones al modelo

--- Institutos/models.py
+++ Institutos/models.py
@@ -1,42 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 class Facilidad(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     
 class FormaPago(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     
 class Comodidad(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     
 class RedSocial(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     
 class Motivo(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
 
 class Barrio(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     
 class Centro(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100)
 
 class Materia(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
     centro = models.ForeignKey(Centro, on_delete=models.PROTECT)
 
 class Instituto(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.PROTECT)
+    id_legado = models.IntegerField(default=0)
+    logo =  models.ImageField(upload_to='images/profile_pics/', default = 'images/profile_pics/blank-profile.jpg')
+    nombre = models.CharField(max_length=50, default='')
+    subtitulo = models.CharField(max_length=100, default='')
+    descripcion = models.CharField(max_length=200, default='')
+    descripcion_corta = models.CharField(max_length=100, default='')
+    telefono = models.CharField(max_length=20, null=True)
+    celular = models.CharField(max_length=20, null=True)
+    email_contacto = models.CharField(max_length=100, default='')
+    direccion = models.CharField(max_length=100, default='')
+    ciudad = models.CharField(max_length=50, default='Montevideo')
+    departamento = models.CharField(max_length=50, default='Montevideo')
+    pais = models.CharField(max_length=50, default='Uruguay')
+    latitud = models.FloatField(default=0)
+    longitud = models.FloatField(default=0)
+    creado = models.DateTimeField(default=datetime.now)
+    modificado = models.DateTimeField(default=datetime.now)
+    posicionamiento = models.IntegerField(default=0)
     facilidades = models.ManyToManyField(Facilidad)
     formasPago = models.ManyToManyField(FormaPago)
     comodidades = models.ManyToManyField(Comodidad)
     barrios = models.ManyToManyField(Barrio)
     facilidades = models.ManyToManyField(Facilidad)
     centros = models.ManyToManyField(Centro)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
 class Profesor(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100)
     instituto = models.ForeignKey(Instituto, on_delete=models.PROTECT)
 
 class Enlace(models.Model):
@@ -47,7 +66,6 @@ class Enlace(models.Model):
     class Meta:
         unique_together = (('instituto', 'redSocial'),)
 
-
 class Transporte(models.Model):
     distancia = models.IntegerField(default=0)
     lineas = models.CharField(max_length=200)
@@ -56,8 +74,8 @@ class Transporte(models.Model):
 class Mensajes(models.Model):
     fecha = models.DateTimeField()
     nombre = models.CharField(max_length=200)
-    telefono = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
     mensaje = models.CharField(max_length=4000)
     motivo = models.ForeignKey(Motivo, on_delete=models.PROTECT)
     instituto = models.ForeignKey(Instituto, on_delete=models.PROTECT, null=True)
