d6206e1bdb17ca740477ac69e4925c7cd55fec84
Andrés Zeballos <andres@topsystemscorp.com>
2018-04-20 19:04:05 -0300

    Actualizacion de /admin

--- Institutos/admin.py
+++ Institutos/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import Facilidad
+from .models import Facilidad, FormaPago, Comodidad, RedSocial, Motivo, Barrio, Centro, Materia, Profesor, Enlace, Transporte, Mensajes
 
 admin.site.register(Facilidad)
+admin.site.register(FormaPago)
+admin.site.register(Comodidad)
+admin.site.register(RedSocial)
+admin.site.register(Motivo)
+admin.site.register(Barrio)
+admin.site.register(Centro)
+admin.site.register(Materia)
+admin.site.register(Profesor)
+admin.site.register(Enlace)
+admin.site.register(Transporte)
+admin.site.register(Mensajes)
