80cb6962c8ff4d42e2ce2c1347b15fd0b8363475
Andrés Zeballos <andres@topsystemscorp.com>
2018-04-24 12:01:13 -0300

    Mejora de ruteo y primer pagina simple. Seteo de linter con django

--- TuProfe/urls.py
+++ TuProfe/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('institutos/', include('Institutos.urls')),
+    path('', include('Institutos.urls')),
     path('admin/', admin.site.urls),
 ]
