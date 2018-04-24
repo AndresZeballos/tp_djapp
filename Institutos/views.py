815f802897b2fd25ca546566937b895b2e6aca5a
Andrés Zeballos <andres@topsystemscorp.com>
2018-04-24 12:01:13 -0300

    Mejora de ruteo y primer pagina simple. Seteo de linter con django

--- Institutos/views.py
+++ Institutos/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Instituto
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hola mundo! Este será la página de bienvenida al sitio")
+
+    
+def buscar(request):
+    institutos = Instituto.objects.order_by('-posicionamiento')[:5]
+    output = ', '.join([i.nombre for i in institutos])
+    return HttpResponse(output)
+
