a9a5a18cec9a965369520765e0fbd2f692e6ffa4
Andrés Zeballos <andres@topsystemscorp.com>
2018-06-14 20:23:04 -0300

    Corrección de usuarios activos

--- Institutos/models.py
+++ Institutos/models.py
@@ -6,6 +6,7 @@ class UsuarioLegado(models.Model):
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=50, null=True)
     password = models.CharField(max_length=32, null=True)
+    activo = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s - %s" % (self.username, self.email)
